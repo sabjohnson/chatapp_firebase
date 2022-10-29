b3d05146bacf0b4a4b486789ef1a9e42ebd3a82c
Sabrina <sabrina.a.johnson@stonybrook.edu>
2022-10-29 10:07:58 -0400

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:49224820880:web:2a8188242445e2e79a9015";
+  static String apiKey = "AIzaSyDKEgMm2ARN2ah054Yd5b8cTDaXoIrizXE";
+  static String messagingSenderId = "49224820880";
+  static String projectId = "chatappflutter-56280";
+  final primaryColor = Color(0xFFee7b64);
+}
